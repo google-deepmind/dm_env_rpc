a81ecc0e18d04f0ffc88d0dea03de1224bce79d8
Tom Ward <tomward@google.com>
2020-06-03 05:45:50 -0700

    Create dm_env_flatten_utils to support the flattening/unflattening of nested actions/observations for dm_env.
    PiperOrigin-RevId: 314701235
    Change-Id: I33126d82b9eab1d06e2b56991dcc3f10a45e4bbc

---
+++ dm_env_rpc/v1/dm_env_flatten_utils.py
@@ -0,0 +1,87 @@
+# Lint as: python3
+# Copyright 2020 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Python utilities for flattening and unflattening key-value mappings."""
+
+import collections
+from typing import Any, Dict, Mapping
+
+
+def flatten_dict(input_dict: Mapping[str, Any],
+                 separator: str) -> Dict[str, Any]:
+  """Flattens mappings by joining sub-keys using the provided separator.
+
+  Only non-empty, mapping types will be flattened. All other types are deemed
+  leaf values.
+
+  Args:
+    input_dict: Mapping of key-value pairs to flatten.
+    separator: Delimiter used to concatenate keys.
+
+  Returns:
+    Flattened dictionary of key-value pairs.
+
+  Raises:
+    ValueError: If the `input_dict` has a key that contains the separator
+      string.
+  """
+  result = collections.OrderedDict()
+  for key, value in input_dict.items():
+    if separator in key:
+      raise ValueError('Key {} already contains separator!'.format(key))
+    if isinstance(value, Mapping) and len(value):
+      result.update({
+          separator.join((key, sub_key)): sub_value
+          for sub_key, sub_value in flatten_dict(value, separator).items()
+      })
+    else:
+      result[key] = value
+  return result
+
+
+def unflatten_dict(input_dict: Mapping[str, Any],
+                   separator: str) -> Dict[str, Any]:
+  """Unflatten dictionary using split keys to determine the structure.
+
+  For each key, split based on the provided separator and create nested
+  dictionary entry for each sub-key.
+
+  Args:
+    input_dict: Mapping of key-value pairs to un-flatten.
+    separator: Delimiter used to split keys.
+
+  Returns:
+    Unflattened dictionary.
+
+  Raises:
+    ValueError: If a key, or it's constituent sub-keys already has a value. For
+      instance, unflattening `{"foo": True, "foo.bar": "baz"}` will result in
+      "foo" being set to both a dict and a Bool.
+  """
+  result = collections.OrderedDict()
+  for key, value in input_dict.items():
+    sub_keys = key.split(separator)
+    sub_tree = result
+    for sub_key in sub_keys[:-1]:
+      sub_tree = sub_tree.setdefault(sub_key, {})
+      if not isinstance(sub_tree, Mapping):
+        raise ValueError(
+            "Sub-tree '{name}' has already been assigned a leaf value {value}"
+            .format(name=sub_key, value=sub_tree))
+
+    if sub_keys[-1] in sub_tree:
+      raise ValueError('Duplicate key {}'.format(key))
+    sub_tree[sub_keys[-1]] = value
+  return result
---
+++ dm_env_rpc/v1/dm_env_flatten_utils_test.py
@@ -0,0 +1,120 @@
+# Lint as: python3
+# Copyright 2020 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Tests for dm_env_flatten_utils."""
+
+import collections
+
+from absl.testing import absltest
+from dm_env_rpc.v1 import dm_env_flatten_utils
+
+
+class FlattenUtilsTest(absltest.TestCase):
+
+  def test_flatten(self):
+    input_dict = {
+        'foo': {
+            'bar': 1,
+            'baz': False
+        },
+        'fiz': object(),
+    }
+    expected = {
+        'foo.bar': 1,
+        'foo.baz': False,
+        'fiz': object(),
+    }
+    self.assertSameElements(expected,
+                            dm_env_flatten_utils.flatten_dict(input_dict, '.'))
+
+  def test_unflatten(self):
+    input_dict = {
+        'foo.bar.baz': True,
+        'fiz.buz': 1,
+        'foo.baz': 'val',
+        'buz': {}
+    }
+    expected = {
+        'foo': {
+            'bar': {
+                'baz': True
+            },
+            'baz': 'val'
+        },
+        'fiz': {
+            'buz': 1
+        },
+        'buz': {},
+    }
+    self.assertSameElements(
+        expected, dm_env_flatten_utils.unflatten_dict(input_dict, '.'))
+
+  def test_unflatten_different_separator(self):
+    input_dict = {'foo::bar.baz': True, 'foo.bar::baz': 1}
+    expected = {'foo': {'bar.baz': True}, 'foo.bar': {'baz': 1}}
+    self.assertSameElements(
+        expected, dm_env_flatten_utils.unflatten_dict(input_dict, '::'))
+
+  def test_flatten_unflatten(self):
+    input_output = {
+        'foo': {
+            'bar': 1,
+            'baz': False
+        },
+        'fiz': object(),
+    }
+    self.assertSameElements(
+        input_output,
+        dm_env_flatten_utils.unflatten_dict(
+            dm_env_flatten_utils.flatten_dict(input_output, '.'), '.'))
+
+  def test_flatten_with_key_containing_separator_raises_error(self):
+    with self.assertRaises(ValueError):
+      dm_env_flatten_utils.flatten_dict({'foo.bar': True}, '.')
+
+  def test_invalid_flattened_dict_raises_error(self):
+    input_dict = collections.OrderedDict((
+        ('foo.bar', True),
+        ('foo', 'invalid_value_for_sub_key'),
+    ))
+    with self.assertRaisesRegex(ValueError, 'Duplicate key'):
+      dm_env_flatten_utils.unflatten_dict(input_dict, '.')
+
+  def test_sub_tree_has_value_raises_error(self):
+    input_dict = collections.OrderedDict((
+        ('branch', 'should_not_have_value'),
+        ('branch.leaf', True),
+        ))
+    with self.assertRaisesRegex(ValueError,
+                                "Sub-tree 'branch' has already been assigned"):
+      dm_env_flatten_utils.unflatten_dict(input_dict, '.')
+
+  def test_empty_dict_values_flatten(self):
+    input_dict = {
+        'foo': {},
+        'bar': {
+            'baz': {}
+        },
+    }
+    expected = {
+        'foo': {},
+        'bar.baz': {},
+    }
+    self.assertSameElements(expected,
+                            dm_env_flatten_utils.flatten_dict(input_dict, '.'))
+
+
+if __name__ == '__main__':
+  absltest.main()
