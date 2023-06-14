5c44ab6943df7d4e3ad371e5ba625deb6916396d
DeepMind <dm-env-rpc-robot@google.com>
2023-06-14 04:51:16 -0700

    Adds utility functions for easily packing/unpacking of DmEnvRpcRequest and DmRpcResponse.
    PiperOrigin-RevId: 540234254
    Change-Id: I773186173aa4399f32b4d1f9ce47842ffe269cfa

--- dm_env_rpc/v1/message_utils.py
+++ dm_env_rpc/v1/message_utils.py
@@ -15,13 +15,15 @@
 """Helper functions used to process dm_env_rpc request / response messages.
 """
 
-from typing import NamedTuple, Union
+import typing
+from typing import Any, Iterable, NamedTuple, Type, Tuple, Union
 
 import immutabledict
 
 from google.protobuf import any_pb2
 from dm_env_rpc.v1 import dm_env_rpc_pb2
 from dm_env_rpc.v1 import error
+from google.protobuf import message
 
 
 _MESSAGE_TYPE_TO_FIELD = immutabledict.immutabledict({
@@ -29,25 +31,131 @@ _MESSAGE_TYPE_TO_FIELD = immutabledict.immutabledict({
     for field in dm_env_rpc_pb2.EnvironmentRequest.DESCRIPTOR.fields
 })
 
-DmEnvRpcRequest = Union[dm_env_rpc_pb2.CreateWorldRequest,
-                        dm_env_rpc_pb2.JoinWorldRequest,
-                        dm_env_rpc_pb2.StepRequest,
-                        dm_env_rpc_pb2.ResetRequest,
-                        dm_env_rpc_pb2.ResetWorldRequest,
-                        dm_env_rpc_pb2.LeaveWorldRequest,
-                        dm_env_rpc_pb2.DestroyWorldRequest,
-                        any_pb2.Any,  # Extension message.
-                       ]
+# An unpacked extension request (anything).
+# As any proto message that is not a native request is accepted, this definition
+# is overly broad - use with care.
+DmEnvRpcExtensionMessage = message.Message
 
-DmEnvRpcResponse = Union[dm_env_rpc_pb2.CreateWorldResponse,
-                         dm_env_rpc_pb2.JoinWorldResponse,
-                         dm_env_rpc_pb2.StepResponse,
-                         dm_env_rpc_pb2.ResetResponse,
-                         dm_env_rpc_pb2.ResetWorldResponse,
-                         dm_env_rpc_pb2.LeaveWorldResponse,
-                         dm_env_rpc_pb2.DestroyWorldResponse,
-                         any_pb2.Any,  # Extension message.
-                        ]
+# A packed extension request.
+# Wraps a DmEnvRpcExtensionMessage.
+DmEnvRpcPackedExtensionMessage = any_pb2.Any
+
+# A native request RPC.
+DmEnvRpcNativeRequest = Union[
+    dm_env_rpc_pb2.CreateWorldRequest,
+    dm_env_rpc_pb2.JoinWorldRequest,
+    dm_env_rpc_pb2.StepRequest,
+    dm_env_rpc_pb2.ResetRequest,
+    dm_env_rpc_pb2.ResetWorldRequest,
+    dm_env_rpc_pb2.LeaveWorldRequest,
+    dm_env_rpc_pb2.DestroyWorldRequest,
+]
+
+# A native response RPC.
+DmEnvRpcNativeResponse = Union[
+    dm_env_rpc_pb2.CreateWorldResponse,
+    dm_env_rpc_pb2.JoinWorldResponse,
+    dm_env_rpc_pb2.StepResponse,
+    dm_env_rpc_pb2.ResetResponse,
+    dm_env_rpc_pb2.ResetWorldResponse,
+    dm_env_rpc_pb2.LeaveWorldResponse,
+    dm_env_rpc_pb2.DestroyWorldResponse,
+]
+
+# A native request RPC, or an extension message, wrapped in Any.
+DmEnvRpcRequest = Union[DmEnvRpcNativeRequest, DmEnvRpcPackedExtensionMessage]
+# A native response RPC, or an extension message, wrapped in Any.
+DmEnvRpcResponse = Union[DmEnvRpcNativeResponse, DmEnvRpcPackedExtensionMessage]
+
+
+def pack_rpc_request(
+    request: Union[DmEnvRpcRequest, DmEnvRpcExtensionMessage],
+) -> DmEnvRpcRequest:
+  """Returns a DmEnvRpcRequest that is suitable to send over the wire.
+
+  Arguments:
+    request: The request to pack.
+
+  Returns:
+    Native request - returned as-is.
+    Packed extension (any_pb2.Any) - returned as-is.
+    Everything else - assumed to be an extension; wrapped in any_pb2.Any.
+  """
+  # pytype: disable=bad-return-type
+  return _pack_message(request, _get_union_args(DmEnvRpcNativeRequest))
+
+
+def unpack_rpc_request(
+    request: Union[DmEnvRpcRequest, DmEnvRpcExtensionMessage],
+    *,
+    extension_type: Union[
+        Type[message.Message], Iterable[Type[message.Message]]
+    ],
+) -> Union[DmEnvRpcRequest, DmEnvRpcExtensionMessage]:
+  """Returns a DmEnvRpcRequest without a wrapper around extension messages.
+
+  Arguments:
+    request: The request to unpack.
+    extension_type: One or more extension protobuf classes of known extension
+      types.
+
+  Returns:
+    Native request - returned as-is.
+    Unpacked extension in |extension_type| - returned as-is.
+    Packed extension (any_pb2.Any) in |extension_type| - returned unpacked
+
+  Raises:
+    ValueError: The message is packed (any_pb2.Any), but not in |extension_type|
+        or the message type is not a native request or known extension.
+  """
+  return _unpack_message(
+      request,
+      _get_union_args(DmEnvRpcNativeRequest),
+      extension_type=extension_type)
+
+
+def pack_rpc_response(
+    response: Union[DmEnvRpcResponse, DmEnvRpcExtensionMessage],
+) -> DmEnvRpcResponse:
+  """Returns a DmEnvRpcResponse that is suitable to send over the wire.
+
+  Arguments:
+    response: The response to pack.
+
+  Returns:
+    Native response - returned as-is.
+    Packed extension (any_pb2.Any) - returned as-is.
+    Everything else - assumed to be an extension; wrapped in any_pb2.Any.
+  """
+  # pytype: disable=bad-return-type
+  return _pack_message(response, _get_union_args(DmEnvRpcNativeResponse))
+
+
+def unpack_rpc_response(
+    response: Union[DmEnvRpcResponse, DmEnvRpcExtensionMessage],
+    *,
+    extension_type: Union[
+        Type[message.Message], Iterable[Type[message.Message]]
+    ],
+) -> Union[DmEnvRpcRequest, DmEnvRpcExtensionMessage]:
+  """Returns a DmEnvRpcResponse without a wrapper around extension messages.
+
+  Arguments:
+    response: The response to unpack.
+    extension_type: One or more extension protobuf classes of known extension
+      types.
+
+  Returns:
+    Native response - returned as-is.
+    Unpacked extension in |extension_type| - returned as-is.
+    Packed extension (any_pb2.Any) in |extension_type| - returned unpacked
+
+  Raises:
+    ValueError: The message is packed (any_pb2.Any), but not in |extension_type|
+        or the message type is not a native request or known extension.
+  """
+  return _unpack_message(response, _get_union_args(DmEnvRpcNativeResponse),
+                         extension_type=extension_type)
 
 
 class EnvironmentRequestAndFieldName(NamedTuple):
@@ -103,3 +211,92 @@ def unpack_environment_response(
   else:
     raise ValueError('Unexpected response message! expected: '
                      f'{expected_field_name}, actual: {response_field_name}')
+
+
+def _pack_message(
+    msg: message.Message,
+    union_types: Tuple[Type[Any], ...],
+) -> message.Message:
+  """Packs a message based on its union types.
+
+  Args:
+    msg: The message to process.
+    union_types: Determines which types are "native" and "extension".
+        "native" types are passed through, unchanged.
+        "extension" types are packed into an Any message and returned.
+
+  Returns:
+    A message within union_types, or an Any proto.
+  """
+  if isinstance(msg, union_types):
+    return msg
+
+  if isinstance(msg, any_pb2.Any):
+    return msg
+
+  # Assume the message is an extension.
+  packed = any_pb2.Any()
+  packed.Pack(msg)
+  return packed
+
+
+def _unpack_message(
+    msg: message.Message,
+    union_types: Tuple[Type[Any], ...],
+    *,
+    extension_type: Union[
+        Type[message.Message], Iterable[Type[message.Message]]
+    ],
+) -> message.Message:
+  """Packs a message based on its union types.
+
+  Args:
+    msg: The message to process.
+    union_types: Determines which types are "native" and "extension". "native"
+      types are passed through, unchanged. "extension" types are packed into an
+      Any message and returned.
+    extension_type: Type or type(s) used to match extension messages. The first
+      matching type is used.
+
+  Returns:
+    An upacked message within |union_types| or an unpacked extension message
+    with type within |extension_type|.
+
+  Raises:
+    TypeError: Raised if a return type could not be determined.
+  """
+  if isinstance(msg, union_types):
+    return msg
+
+  if isinstance(extension_type, type):
+    extension_type = (extension_type,)
+  else:
+    extension_type = tuple(extension_type)
+
+  if isinstance(msg, extension_type):
+    return msg
+
+  if isinstance(msg, any_pb2.Any):
+    matching_type = next(
+        (e for e in extension_type if msg.Is(e.DESCRIPTOR)), None)
+
+    if not matching_type:
+      raise ValueError(
+          'Extension type could not be found to unpack message: '
+          f'{type(msg).__name__}.\n'
+          f'Known Types:\n' + '\n'.join(f'- {e}' for e in extension_type))
+
+    unpacked = matching_type()
+    msg.Unpack(unpacked)
+    return unpacked
+
+  raise ValueError(
+      'Message type does not appear in union_types and is not an extension: '
+      f'{type(msg).__name__}.\n'
+      f'Union Types:\n' + '\n'.join(f'- {t}' for t in union_types))
+
+
+def _get_union_args(union) -> Tuple[Type[Any], ...]:
+  """Python version-safe function for getting the types in a union."""
+  # __args__ exists < v3.8, typing.get_args() exists >= 3.8.
+  return getattr(union, '__args__', None) or typing.get_args(union)
--- dm_env_rpc/v1/message_utils_test.py
+++ dm_env_rpc/v1/message_utils_test.py
@@ -15,12 +15,14 @@
 """Tests for dm_env_rpc/message_utils."""
 
 from absl.testing import absltest
+from absl.testing import parameterized
 
 from google.rpc import status_pb2
 from dm_env_rpc.v1 import dm_env_rpc_pb2
 from dm_env_rpc.v1 import error
 from dm_env_rpc.v1 import message_utils
 from dm_env_rpc.v1 import tensor_utils
+from google.protobuf import any_pb2
 
 
 _CREATE_WORLD_REQUEST = dm_env_rpc_pb2.CreateWorldRequest(
@@ -29,8 +31,15 @@ _CREATE_WORLD_RESPONSE = dm_env_rpc_pb2.CreateWorldResponse(world_name='qux')
 _CREATE_WORLD_ENVIRONMENT_RESPONSE = dm_env_rpc_pb2.EnvironmentResponse(
     create_world=_CREATE_WORLD_RESPONSE)
 
+# Anything that's not a "native" rpc message is an extension.
+_EXTENSION_TYPE = error.status_pb2.Status
+_EXTENSION_MULTI_TYPE = (dm_env_rpc_pb2.EnvironmentResponse, _EXTENSION_TYPE)
+_EXTENSION_MESSAGE = _EXTENSION_TYPE(code=42)
+_PACKED_EXTENSION_MESSAGE = any_pb2.Any()
+_PACKED_EXTENSION_MESSAGE.Pack(_EXTENSION_MESSAGE)
 
-class MessageUtilsTests(absltest.TestCase):
+
+class MessageUtilsTests(parameterized.TestCase):
 
   def test_pack_create_world_request(self):
     environment_request, field_name = message_utils.pack_environment_request(
@@ -63,6 +72,113 @@ class MessageUtilsTests(absltest.TestCase):
               leave_world=dm_env_rpc_pb2.LeaveWorldResponse()),
           'create_world')
 
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='create_world_request_passes_through',
+          message=_CREATE_WORLD_REQUEST,
+          expected=_CREATE_WORLD_REQUEST,
+      ),
+      dict(
+          testcase_name='packed_extension_message_passes_through',
+          message=_PACKED_EXTENSION_MESSAGE,
+          expected=_PACKED_EXTENSION_MESSAGE,
+      ),
+      dict(
+          testcase_name='extension_message_is_packed',
+          message=_EXTENSION_MESSAGE,
+          expected=_PACKED_EXTENSION_MESSAGE,
+      ),
+  )
+  def test_pack_request(self, message, expected):
+    self.assertEqual(message_utils.pack_rpc_request(message), expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='create_world_response',
+          message=_CREATE_WORLD_RESPONSE,
+          expected=_CREATE_WORLD_RESPONSE,
+      ),
+      dict(
+          testcase_name='extension_message',
+          message=_EXTENSION_MESSAGE,
+          expected=_PACKED_EXTENSION_MESSAGE,
+      ),
+      dict(
+          testcase_name='packed_extension_message',
+          message=_PACKED_EXTENSION_MESSAGE,
+          expected=_PACKED_EXTENSION_MESSAGE,
+      ),
+  )
+  def test_pack_response(self, message, expected):
+    self.assertEqual(message_utils.pack_rpc_response(message), expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='create_world_request_passes_through',
+          message=_CREATE_WORLD_REQUEST,
+          extensions=_EXTENSION_TYPE,
+          expected=_CREATE_WORLD_REQUEST,
+      ),
+      dict(
+          testcase_name='extension_message_passes_through',
+          message=_EXTENSION_MESSAGE,
+          extensions=_EXTENSION_TYPE,
+          expected=_EXTENSION_MESSAGE,
+      ),
+      dict(
+          testcase_name='extension_message_passes_through_with_multi',
+          message=_EXTENSION_MESSAGE,
+          extensions=_EXTENSION_MULTI_TYPE,
+          expected=_EXTENSION_MESSAGE,
+      ),
+      dict(
+          testcase_name='packed_extension_message_is_unpacked_with_multi',
+          message=_PACKED_EXTENSION_MESSAGE,
+          extensions=_EXTENSION_MULTI_TYPE,
+          expected=_EXTENSION_MESSAGE,
+      ),
+  )
+  def test_unpack_request(self, message, extensions, expected):
+    self.assertEqual(
+        message_utils.unpack_rpc_request(message, extension_type=extensions),
+        expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='create_world_response_passes_through',
+          message=_CREATE_WORLD_RESPONSE,
+          extensions=_EXTENSION_TYPE,
+          expected=_CREATE_WORLD_RESPONSE,
+      ),
+      dict(
+          testcase_name='extension_message_passes_through',
+          message=_EXTENSION_MESSAGE,
+          extensions=_EXTENSION_TYPE,
+          expected=_EXTENSION_MESSAGE,
+      ),
+      dict(
+          testcase_name='packed_extension_message_is_unpacked',
+          message=_PACKED_EXTENSION_MESSAGE,
+          extensions=_EXTENSION_TYPE,
+          expected=_EXTENSION_MESSAGE,
+      ),
+      dict(
+          testcase_name='extension_message_passes_through_with_multi',
+          message=_EXTENSION_MESSAGE,
+          extensions=_EXTENSION_MULTI_TYPE,
+          expected=_EXTENSION_MESSAGE,
+      ),
+      dict(
+          testcase_name='packed_extension_message_is_unpacked_with_multi',
+          message=_PACKED_EXTENSION_MESSAGE,
+          extensions=_EXTENSION_MULTI_TYPE,
+          expected=_EXTENSION_MESSAGE,
+      ),
+  )
+  def test_unpack_response(self, message, extensions, expected):
+    self.assertEqual(
+        message_utils.unpack_rpc_response(message, extension_type=extensions),
+        expected)
 
 if __name__ == '__main__':
   absltest.main()
